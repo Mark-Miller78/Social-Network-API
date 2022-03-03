50b325f135e13c5f989997ee2cac718f0d6a821f
Mark Miller <markmiller7858@gmail.com>
2022-03-03 15:16:47 -0500

    reaction subschema setup

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,36 @@
 const {Schema, model, Types} = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const reactionSchema = new Schema(
+    {
+        reactionId:{
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody:{
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        username:{
+            type: String,
+            required: true,
+            ref: 'User'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJson:{
+            getters: true
+        }
+    }
+);
+
 const thoughtSchema = new Schema(
     {
         thoughtText:{
@@ -34,3 +64,7 @@ const thoughtSchema = new Schema(
 thoughtSchema.virtual('reactionCount').get(function(){
     return this.reactions.length;
 });
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
