64bdd7e52fb9ff0fbdb172471e4a51c811e5d632
Mark Miller <markmiller7858@gmail.com>
2022-03-06 16:50:22 -0500

    friends added and deleted

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,9 +15,13 @@ const userController={
     getUserById({params}, res){
         User.findOne({_id: params.id})
             .populate({
-                path: 'thoughts',
-                select:('-__v')
+                    path: 'thoughts',
+                    select: ('-__v')
             })
+            .populate({
+                path: 'friends',
+                select: ('-__V')
+            })       
             .select('-__v')
             .then(dbUserData => {
                 if(!dbUserData){
@@ -66,6 +70,44 @@ const userController={
                 console.log(err);
                 res.status(400).json(err);
             })
+    },
+
+    addFriend({params}, res){
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            {$push: {friends: params.friendId}},
+            {new: true}
+        )
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(404).json({message: 'No User found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    },
+
+    deleteFriend({params}, res){
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            {$pull: {friends: params.friendId}},
+            {new: true}
+        )
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(404).json({message: 'No User found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
     }
 }
 
